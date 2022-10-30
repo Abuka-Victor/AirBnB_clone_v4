ca1c36e4228863fe5dce2fae4d3c0d1e099048c2
Abuka-Victor <abukavictoro@gmail.com>
2022-10-30 11:47:24 +0100

    Task 4: Fetch places client-side rather than server-side

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -32,4 +32,36 @@ $('html').ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
+
+  let places_list;
+
+  $.ajax({
+    url: "http://0.0.0.0:5001/api/v1/places_search/",
+    headers: {"Content-Type": "application/json"},
+    data: JSON.stringify({}),
+    context: $("section.places"),
+    success: function (responses) {
+     places_list = responses.map(function(res) {
+       return `
+           <article>
+               <div class="title_box">
+                   <h2>${res.name}</h2>
+                   <div class="price_by_night">${res.price_by_night}</div>
+               </div>
+               <div class="information">
+                   <div class="max_guest">${res.max_guest} Guest(s)</div>
+                   <div class="number_rooms">${res.number_rooms} Bedroom(s)</div>
+                   <div class="number_bathrooms">${res.number_bathrooms} Bathroom(s)</div>
+               </div>
+               <div class="description">
+                   ${res.description}
+               </div>
+           </article>
+       `
+     })
+ }
+}).done(function() {
+    $(this).html(places_list.join(''));
 });
+}
+
